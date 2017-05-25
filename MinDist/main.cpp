4b1a438666b886586ddc25f119ad739e43a0d94c
SaiWhiley <sai.whiley196@gmail.com>
2017-05-25 19:09:47 +1000

    Sai makes all the code

--- MinDist/main.cpp
+++ MinDist/main.cpp
@@ -17,22 +17,71 @@
 
 using namespace std;
 using namespace std::chrono;
-mt19937 random_engine(time(0));
+
 
 vector<int> MakeRandomArray(int numElements);
 tuple<float,long> MinDistance(vector<int> numbers);
 tuple<float, long> MinDistance2(vector<int> numbers);
+tuple<float, float> MinDistTIME(vector<int> numbers);
+tuple<float, float> MinDist2TIME(vector<int> numbers);
+void runBasicOperations(int increments, int trials);
 string ConvertArrayToString(vector <int> Array);
 
 typedef vector<tuple<float,int>>listOfOperationResults;
+typedef vector<tuple<float,float>>listOfTimeResults;
 
 int main(){
     cout << "Assessment 1: MinDist | Liam Abel n9467874 & Sai Whiley n9454829" << endl;
-    vector <int> results = {};
-    vector<int> results2 = {};
+    runBasicOperations(100, 10);
+
+}
+
+void runTimeElapsed(int increments, int trials){
     int numValues = 0;
-    int increments = 100;
-    int trials = 10;
+    vector<int> results = {};
+    vector<int> results2 = {};
+    float totalRuntimeONE;
+    float meanRuntimeONE;
+    float totalRuntimeTWO;
+    float meanRuntimeTWO;
+
+    listOfTimeResults timeResults1;
+    listOfTimeResults timeResults2;
+
+    ofstream resultsStream;
+    resultsStream.open("timeResults.csv", fstream::app);
+    resultsStream << ",Mean Time Elapsed, \r";
+    resultsStream << "Array Size, ONE, TWO";
+
+    for(int i = 0; i < increments; i++){
+        numValues +=10;
+        timeResults1.clear();
+        timeResults2.clear();
+        totalRuntimeONE = 0;
+        meanRuntimeONE =0;
+        totalRuntimeTWO=0;
+        meanRuntimeTWO=0;
+
+        for(int j = 0; j < trials; j++){
+            timeResults1.emplace_back(MinDistTIME(MakeRandomArray(numValues)));
+            timeResults2.emplace_back(MinDist2TIME(MakeRandomArray(numValues)));
+        }
+        for(auto &n : timeResults1){
+            totalRuntimeONE += get<1>(n);
+        }
+        for(auto &n : timeResults2){
+            totalRuntimeTWO += get<1>(n);
+        }
+        meanRuntimeONE = totalRuntimeONE/trials;
+        meanRuntimeTWO = totalRuntimeTWO/trials;
+
+        resultsStream << "\r" << numValues << "," << meanRuntimeONE << "," << meanRuntimeTWO;
+    }
+
+}
+
+void runBasicOperations(int increments, int trials){
+    int numValues = 0;
     long operationsTotalONE;
     long meanOperationsONE;
     long operationsTotalTWO;
@@ -48,6 +97,7 @@ int main(){
     for(int i = 0; i < increments; i++){
         numValues +=10;
         opResults1.clear();
+        opResults2.clear();
         meanOperationsONE = 0;
         operationsTotalONE = 0;
         meanOperationsTWO = 0;
@@ -71,8 +121,8 @@ int main(){
 
 
     }
-
 }
+
 
 string ConvertArrayToString(vector <int> Array){
     string result = "";
@@ -83,6 +133,7 @@ string ConvertArrayToString(vector <int> Array){
 }
 
 vector<int> MakeRandomArray(int numElements){
+    mt19937 random_engine(time(0));
     vector<int> testArray;
     for(int i=0; i<numElements; i++){
         testArray.push_back(random_engine()%100);
@@ -95,8 +146,8 @@ tuple<float,long> MinDistance(vector<int> numbers){  //takes vector and returns 
     double dmin = numeric_limits<double>::max();
     for(int i=0;i<numbers.size()-1;i++){
         for(int j=0;j<numbers.size()-1;j++){
-            basicOperations++;
-            if(i!=j&&abs(numbers[i]-numbers[j]) < dmin){
+            basicOperations+= 2;
+            if(i!=j&&abs(numbers[i]-numbers[j]) < dmin){ //2 comparison operations
                 dmin = abs(numbers[i]-numbers[j]);
             }
         }
@@ -105,20 +156,53 @@ tuple<float,long> MinDistance(vector<int> numbers){  //takes vector and returns 
     return make_tuple(dmin, basicOperations);
 }
 
-tuple<float, long> MinDistance2(vector<int> numbers); //takes ve4c
-(vector<int> numbers){
+tuple<float, long> MinDistance2(vector<int> numbers){ //takes ve4c
     long int basicOperations = 0;
     double temp = 0;
     double dmin = numeric_limits<double>::max();
     for(int i=0;i<numbers.size()-2;i++){
         for(int j=0;j<numbers.size()-1;j++){
-            basicOperations++;
             temp = abs(numbers[i] - numbers[j]);
+            basicOperations++;
             if(temp < dmin){
-                basicOperations++;
                 dmin = temp;
             }
         }
     }
     return make_tuple(dmin, basicOperations);
+}
+
+tuple<float, float> MinDistTIME(vector<int> numbers){
+    double dmin = numeric_limits<double>::max();
+    duration<double> timeElapsed;
+    steady_clock::time_point startTime  = steady_clock::now();
+    for(int i=0;i<numbers.size()-1;i++){
+        for(int j=0;j<numbers.size()-1;j++){
+            if(i!=j&&abs(numbers[i]-numbers[j]) < dmin){ //2 comparison operations
+                dmin = abs(numbers[i]-numbers[j]);
+            }
+        }
+    }
+    steady_clock::time_point endTime = steady_clock::now();
+    timeElapsed = duration_cast<duration<double>>(endTime - startTime);
+    cout << "array size: " << numbers.size() << " mindist: " << "time taken: " << timeElapsed.count() << endl;
+    return make_tuple(dmin, timeElapsed.count());
+}
+
+tuple<float, float> MinDist2TIME(vector<int> numbers){
+    double temp = 0;
+    double dmin = numeric_limits<double>::max();
+    duration<double> timeElapsed;
+    steady_clock::time_point startTime = steady_clock::now();
+    for(int i=0;i<numbers.size()-2;i++){
+        for(int j=0;j<numbers.size()-1;j++){
+            temp = abs(numbers[i] - numbers[j]);
+            if(temp < dmin){
+                dmin = temp;
+            }
+        }
+    }
+    steady_clock::time_point endTime = steady_clock::now();
+    timeElapsed = duration_cast<duration<double>>(endTime - startTime);
+    return make_tuple(dmin, timeElapsed.count());
 }
